257465cac60f9f266b8c89e6949b060bd502a841
rajeevbrahma <rajeevbrahma@hotmail.com>
2016-07-19 16:22:14 +0530

    new

--- rp_blu.py
+++ rp_blu.py
@@ -5,13 +5,11 @@ import time
 import json
 
 
-organization = "5q764p"
-appId = "APP"
-authMethod = "apikey"
-authKey = "a-5q764p-bpnugpigze"
-authToken = "JZ4YT5_*n+9OWXw9*w"
-deviceType = "Trashcan"
-deviceId = "APP"
+# External module imports                                                                                                        
+import RPi.GPIO as GPIO                                                                                                          
+import time                                                                                                                      
+import dweepy                                                                                                                    
+from sht21 import SHT21                                                                                                          
 
 options = {"org": organization, "id":appId, "auth-method": authMethod, "auth-key": authKey, "auth-token": authToken}
 
@@ -19,16 +17,9 @@ client = ibmiotf.application.Client(options)
 
 client.connect()
 
-
 temparatureReading = 0
 humidityReading = 0
 
-
-# External module imports                                                                                                        
-import RPi.GPIO as GPIO                                                                                                          
-import time                                                                                                                      
-import dweepy                                                                                                                    
-from sht21 import SHT21                                                                                                          
                 
 sender = 'blue-iot'                                                                                                              
 # Pin Definitons:                                                                                                                
@@ -60,39 +51,39 @@ GPIO.output(alarmOut, GPIO.LOW)
                                 
 
                                                                                                  
-                                                                                                                                 
-                                                                                                                                 
+                                                                                                                     
+                                                                                                                     
 print("Here we go! Press CTRL+C to exit")                                                                                        
 try:                                                                                                                             
-    while 1:                                                                                                                     
-        alarm = False                                                                                                            
-        myDweet['DoorAlarm'] = False                                                                                             
-        myDweet['WindowAlarm'] = False                                                                                           
-        myDweet['FireAlarm'] = False                                                                                             
-        myDweet['WaterAlarm'] = False 
+	while 1:                                                                                                                     
+		alarm = False                                                                                                            
+		myDweet['DoorAlarm'] = False                                                                                             
+		myDweet['WindowAlarm'] = False                                                                                           
+		myDweet['FireAlarm'] = False                                                                                             
+		myDweet['WaterAlarm'] = False 
 		if GPIO.input(doorSensor): # button is released                                                                          
-            myDweet['DoorAlarm'] = True                                                                                          
-            alarm = True                                                                                                         
-        if GPIO.input(windowSensor): # button is released                                                                        
-            myDweet['WindowAlarm'] = True                                                                                        
-            alarm = True                                                                                                         
-        if sht.read_temperature() > 100: #excessive heat
-        	temparatureReading = sht.read_temperature()
-            myDweet['FireAlarm'] = True                                                                                          
-            alarm = True                                                                                                         
-        if sht.read_humidity() > 80: #abnormal humidity
-        	humidityReading = sht.read_humidity()                                                                           
-            myDweet['WaterAlarm'] = True                                                                                         
-            alarm = True                                                                                                         
-                                                                                                                                 
-        if alarm:                                                                                                                
-            GPIO.output(alarmOut, GPIO.HIGH)                                                                                     
-        else:                                                                                                                    
-            GPIO.output(alarmOut, GPIO.LOW)                                                                                      
-        message = {"temparatureReading":temparatureReading,"humidityReading":humidityReading,"DoorAlarm":GPIO.input(doorSensor),"WindowAlarm":GPIO.input(windowSensor)}  
- 		client.publishEvent(deviceType, deviceId, "status", "json", message)
-		                                                                                                                              
-        time.sleep(1)                                                                                                            
-        dweepy.dweet_for(sender, myDweet );                                                                                      
+			myDweet['DoorAlarm'] = True                                                                                          
+			alarm = True                                                                                                         
+		if GPIO.input(windowSensor): # button is released                                                                        
+			myDweet['WindowAlarm'] = True                                                                                        
+			alarm = True                                                                                                         
+		if sht.read_temperature() > 100: #excessive heat
+			temparatureReading = sht.read_temperature()
+			myDweet['FireAlarm'] = True                                                                                          
+			alarm = True                                                                                                         
+		if sht.read_humidity() > 80: #abnormal humidity
+			humidityReading = sht.read_humidity()                                                                           
+			myDweet['WaterAlarm'] = True                                                                                         
+			alarm = True                                                                                                         
+		                                                                                                                     
+		if alarm:                                                                                                                
+			GPIO.output(alarmOut, GPIO.HIGH)                                                                                     
+		else:                                                                                                                    
+			GPIO.output(alarmOut, GPIO.LOW)                                                                                      
+		message = {"temparatureReading":temparatureReading,"humidityReading":humidityReading,"DoorAlarm":GPIO.input(doorSensor),"WindowAlarm":GPIO.input(windowSensor)}  
+		client.publishEvent(deviceType, deviceId, "status", "json", message)
+		                                                                                                                          
+		time.sleep(1)                                                                                                            
+		dweepy.dweet_for(sender, myDweet );                                                                                      
 except KeyboardInterrupt: # If CTRL+C is pressed, exit cleanly:                                                                  
-    GPIO.cleanup() # cleanup all GPIO    
+	GPIO.cleanup() # cleanup all GPIO    
